887e4123e847ea6868bf0fd9d1959b0492d16cea
regone7 <regone12345@gmail.com>
2024-06-13 02:50:59 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import pic1 from './assets/picture/pic1.png'
 import pic2 from './assets/picture/pic2.png'
 import pic3 from './assets/picture/pic3.png'
 import pic4 from './assets/picture/pic4.png'
+import pic6 from './assets/picture/pic6.png'
 import primam1 from './assets/picture/primam1.png'
 import primam2 from './assets/picture/primam2.png'
 import primam3 from './assets/picture/primam3.png'
@@ -18,6 +19,12 @@ import whyprim1 from './assets/picture/whyprim1.png'
 import whyprim2 from './assets/picture/whyprim2.png'
 import whyprim3 from './assets/picture/whyprim3.png'
 import whyprim4 from './assets/picture/whyprim4.png'
+import whyprim5 from './assets/picture/whyprim5.png'
+import whyprim6 from './assets/picture/whyprim6.png'
+import userpic1 from './assets/picture/userpic1.png'
+import userpic2 from './assets/picture/userpic2.png'
+import userpic3 from './assets/picture/userpic3.png'
+
 
 
 
@@ -30,7 +37,7 @@ function App() {
 
       <div className='container mx-auto'>
         {/* navbar */}
-        <nav className='w-[350px] md:w-full h-[150px] md:h-full'>
+        <nav className='w-[350px] md:w-full h-[150px] md:h-full p-5'>
           <div className='flex flex-col md:flex-row justify-between items-center h-16 gap-3'>
             <div >
               <img src={logo} alt="" />
@@ -54,7 +61,7 @@ function App() {
         {/*make the best financial decisions section  */}
         <section>
           <div className='flex flex-col md:flex-row '>
-            <div className='p-5 md:mt-12 md:w-[45%] '>
+            <div className='p-5 md:mt-16 md:w-[45%] bg-no-repeat bg-top'  style={{ backgroundImage: `url(${pic6})` }} >
               <h1 className=' md:text-[40px] text-[30px] font-bold '> Make the best Financial Decisions</h1>
               <p className='md:mt-3 md:mb-1'>Cum et convallis risus placerat aliquam, nunc. Scelerisque aliquet faucibus tincidunt eu adipiscing sociis arcu lorem porttitor.</p>
               <div>
@@ -81,12 +88,12 @@ function App() {
         {/* uifry premium section */}
         <section>
           <div className='flex flex-col md:flex-row'>
-            <div >
-              <div>
+            <div className='bg-no-repeat bg-top  ' style={{ backgroundImage: `url(${whyprim5})` }} >
+              <div >
                 <img src={primam1} alt="" />
               </div>
             </div>
-            <div className='p-5'>
+            <div className='p-5 '>
               <div className='md:mt-9'>
                 <p className='text-xl text-orange-500'>Features</p>
                 <h1 className='text-[32px] font-semibold'>Uifry Premium</h1>
@@ -145,12 +152,12 @@ function App() {
               </div>
               <p className='text-gray-500'>Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris etiam odio. Duis tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget ac dolor neque lorem sapien, suspendisse aliquam.</p>
             </div>
-            <div className='md:w-[50%] h-full'>
+            <div className='md:w-[50%] h-full bg-no-repeat bg-top' style={{ backgroundImage: `url(${whyprim5})` }}>
               <img src={whyprim2} alt="" />
             </div>
           </div>
           <div className='flex flex-col md:flex-row'>
-            <div className='md:w-[50%] h-full'>
+            <div className='md:w-[50%] h-full bg-no-repeat bg-top' style={{ backgroundImage: `url(${whyprim6})` }}>
               <img src={whyprim3} alt="" />
             </div>
             <div className='p-5 md:w-[50%] md:h-[500px] flex flex-col  justify-center  '>
@@ -163,6 +170,29 @@ function App() {
                 </div>
               </div>
               <p className='text-gray-500'>Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris etiam odio. Duis tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget ac dolor neque lorem sapien, suspendisse aliquam.</p>
+            </div>
+          </div>
+        </section>
+        {/* what our users say about us? section */}
+        <section>
+          <div className='text-center '>
+            <p >Testimonial</p>
+            <h1 className='md:text-[42px] font-semibold text-2xl'>what our users say </h1>
+            <h1 className='md:text-[42px] font-semibold text-2xl' >about us? </h1>
+          </div>
+          <div className='flex flex-col md:flex-row md:-mt-7 p-5'>
+            <div>
+              <div className='bg-no-repeat bg-left' style={{ backgroundImage: `url(${userpic3})` }}>
+                <img src={userpic1} alt="" />
+              </div>
+            </div>
+            <div className='md:mt-24 p-5 md:w-[48%]'>
+              <h1 className='text-2xl'>The best financial accounting app ever!</h1>
+              <p className='my-3 text-gray-500'>“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris etiam odio. Duis tristique lacus, et blandit viverra nisl velit. Sed mattis rhoncus, diam suspendisse sit nunc, gravida eu. Lectus eget eget ac dolor neque lorem sapien, suspendisse aliquam.”</p>
+              <div >
+                <img  src={userpic2} alt="" />
+              </div>
+              <p className='my-3 text-gray-500'>nick jonas</p>
             </div>
           </div>
         </section>
